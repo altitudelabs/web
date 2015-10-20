95fe0c31046c09db9e0528bb299981bbce454a3c
dcheong300 <cheong.derekb@gmail.com>
2015-10-20 14:49:45 +0800

    Could not finish in 1 hour

---
+++ js/script.js
@@ -0,0 +1,17 @@
+(function(){
+
+	var poller = new massrel.Poller( //options, callback
+		{
+			frequency: 15
+			limit: 5
+		},
+		
+		//callback = data (name, count)
+		
+	)
+	
+
+
+
+
+}(jQuery));
